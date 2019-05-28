be63aa0d9b3c5b7f98b33c040d5c537bdfe0873d
Sneguro4ka <yvova88@yahoo.com>
2019-05-28 17:12:04 +0300

    dom tree, first touch

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react'
 import './App.css';
 import Test from './components/Test'
 import { observer } from 'mobx-react'
+import Board from './components/Board/Board';
+import Player from './components/Player/Player';
+import BoardControls from './components/Board/BoardControls';
 
 @observer
 class App extends Component {
@@ -10,6 +13,12 @@ class App extends Component {
     return (
       <div className="App">
         <Test />
+        <div className="GameScreen">
+        <Player player="player1"/> 
+        <Player player="player1"/> 
+        <BoardControls />
+        <Board /> 
+        </div>
       </div>
     )
   }
