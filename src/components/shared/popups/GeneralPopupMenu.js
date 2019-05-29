d3a94a6124123b30798b021bf7365da31954ba77
Sneguro4ka <yvova88@yahoo.com>
2019-05-29 18:04:29 +0300

    showing popup menu depending on type of tile

--- src/components/shared/popups/GeneralPopupMenu.js
+++ src/components/shared/popups/GeneralPopupMenu.js
@@ -2,26 +2,34 @@ import React, { Component } from 'react';
 import CombatPopup from './CombatPopup';
 import { observer, inject } from 'mobx-react'
 
-@inject( "gameStore")
+@inject("gameStore")
 
 @observer
 class GeneralPopupMenu extends Component {
     drawCard = () => {
-
-        let tileTypeFromStore = this.props.gameStore.getTileType()
-        console.log(tileTypeFromStore)
         console.log("drawing card...")
     }
     buyStuff = () => {
-        console.log("buying gold...")
+        console.log("buying stuff...")
     }
-    
+    renderfieldOptions = () =>
+        this.props.gameStore.getTileType() === "Fields" ? <button onClick={this.drawCard}>Draw Card2</button> : null
+
+    renderVillageOptions = () =>
+        this.props.gameStore.getTileType() === "Village" ? <button onClick={this.buyStuff}>Buy Stuff2</button> : null
+
     render() {
         return (
             <div className="general-popup-container">
-                {this.props.gameStore.game.isBattleOn ? null : <CombatPopup />}
-                <button onClick={this.drawCard}>Draw Card</button>
-                <button onClick={this.buyStuff}>Buy Stuff</button>
+                {
+                    this.props.gameStore.game.isBattleOn ?
+                    <CombatPopup />
+                    :
+                    <div>
+                        {this.renderfieldOptions()}
+                        {this.renderVillageOptions()}
+                    </div>
+                }
             </div>
         )
     }
