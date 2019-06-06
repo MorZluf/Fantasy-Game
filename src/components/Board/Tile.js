ed1d607915af06e837754802422edecf78703d37
Sneguro4ka <yvova88@yahoo.com>
2019-06-06 10:22:15 +0300

    pl1 pl2 imgs

--- src/components/Board/Tile.js
+++ src/components/Board/Tile.js
@@ -9,12 +9,29 @@ class Tile extends Component {
 
     movePlayer = () => this.props.gameStore.movePlayer(this.props.coords)
 
-    render(){
+    renderClassNameByPlayer = () => {
+        let className = ""
         let tile = this.props.tileData
-        return (<div onClick={this.movePlayer} 
+        if (tile.players.length > 0)
+            tile.players.map(p => {
+                if (p === "Player_1")
+                    className = "player1_icon"
+                else
+                    className = "player2_icon"
+            })
+
+        return className
+    }
+    render() {
+        let tile = this.props.tileData
+        return (<div onClick={this.movePlayer}
             className={tile.canMoveHere ? `valid-move ${tile.type} tile` : `${tile.type} tile`}>
-            {tile.type} 
-            {tile.players.length > 0 ? <div>{tile.players.map(p => p)}</div> : null}
+            {tile.type}
+            {/* The below one is without img. */}
+            {/* {tile.players.length > 0 ? <div>{tile.players.map(p => p)}</div> : null}  */}
+
+            {/* The below oone is with img */}
+            {tile.players.length > 0 ? <div className={this.renderClassNameByPlayer()}></div> : null}
         </div>)
     }
 }
